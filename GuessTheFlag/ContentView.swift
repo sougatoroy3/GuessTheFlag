9976782c3019b1da0b4f1e78dcb1285ca03ca996
sougatoroy3 <sougatoroy007@gmail.com>
2025-07-28 02:17:25 +0530

    State of variables and Alert notifier added

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userScore = 0
+    @State private var showingAlert = false
+    
+    var finalScore : Int{
+        let score = userScore
+        // code to check correct answer
+        // code to add or subtract points
+        
+        return score
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Show Score"){
+            showingAlert = true
         }
-        .padding()
+        .alert("Correct Answer", isPresented: $showingAlert){
+            Button("OK"){}
+        }message:{
+            Label("Score: \(finalScore)", systemImage: "info.circle")
+        }
+        
     }
 }
 
